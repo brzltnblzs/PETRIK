7052999f8127e59d41ed43dede19c49390702573
bzrltnblzs <biro.zoltan.balazs@gmail.com>
2022-10-27 12:11:08 +0200

    Feltöltés

--- Programozás/2022/Október/18/fel.py
+++ Programozás/2022/Október/18/fel.py
@@ -1,4 +1,5 @@
 import random
+from sys import api_version
 
 
 def f01():
@@ -46,15 +47,61 @@ def f06():
     szavak=[]
     for i in range(5):
         szavak.append(input("Kérem a szavakat: "))
+    szavakbackup=[]
+    szavakbackup.append(szavak)
     reszlet=str(input("Részlet: "))
     for i in range(len(szavak)):
         if szavak[i].find(reszlet) == 0:
             a=a+1
     print(a)
-
-
-
-
+    print("---------------")
+    szavak.sort()
+    for i in range (4):
+        szavak.pop(0)
+    print(*szavak)
+    print("---------------")
+    szavak.append(szavakbackup)
+    for i in range (1):
+        szavak1=szavak[0]
+        c1=szavak1.count("a")
+        c2=szavak1.count("e")
+        c3=szavak1.count("i")
+        c4=szavak1.count("o")
+        c5=szavak1.count("u")
+        print("Az első szóban", c1+c2+c3+c4+c5 ,"darab magánhangzó van!")
+    for i in range (1):
+        szavak2=szavak[1]
+        c1=szavak2.count("a")
+        c2=szavak2.count("e")
+        c3=szavak2.count("i")
+        c4=szavak2.count("o")
+        c5=szavak2.count("u")
+        print("A második szóban", c1+c2+c3+c4+c5 ,"darab magánhangzó van!")
+    for i in range (1):
+        szavak3=szavak[2]
+        c1=szavak3.count("a")
+        c2=szavak3.count("e")
+        c3=szavak3.count("i")
+        c4=szavak3.count("o")
+        c5=szavak3.count("u")
+        print("A harmadik szóban", c1+c2+c3+c4+c5 ,"darab magánhangzó van!")
+    for i in range (1):
+        szavak4=szavak[3]
+        c1=szavak4.count("a")
+        c2=szavak4.count("e")
+        c3=szavak4.count("i")
+        c4=szavak4.count("o")
+        c5=szavak4.count("u")
+        print("A negyedik szóban", c1+c2+c3+c4+c5 ,"darab magánhangzó van!")
+    for i in range (1):
+        szavak5=szavak[4]
+        c1=szavak5.count("a")
+        c2=szavak5.count("e")
+        c3=szavak5.count("i")
+        c4=szavak5.count("o")
+        c5=szavak5.count("u")
+        print("Az ötödik szóban", c1+c2+c3+c4+c5 ,"darab magánhangzó van!")
+    
 def main():
     #f01()
     #f02()
