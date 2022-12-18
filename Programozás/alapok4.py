109e9c31da5ceb0dc3545e0f5ef7a13be95098ad
brzltnblzs <biro.zoltan.balazs@gmail.com>
2022-12-18 18:23:41 +0100

    Feltöltés

--- Programozás/alapok4.py
+++ Programozás/alapok4.py
@@ -1,22 +1,33 @@
-def main():
-    n=int(input("Add meg a számod: "))
-    e=rajz(n)
-    print(e)
+# simple calculator
 
+def operations():
+    print("1 - addition")
+    print("2 - subtract")
+    print("3 - multiply")
+    print("4 - division")
 
-def rajz(n):
-    m=""
-    for i in range(n+1):
-        s=""
-        for j in range (n+1):
-            if(i==0):
-                s=s+f"{j}\t"
-            elif(j==0):
-                s=s+f"{i}\t"
-            else:
-                s=s+f"{i*j}\t"
-        m=m+s+"\n"
-    return m
+def calculator():
+    valasztott = int(input("Please, select your operation!: "))
+    while valasztott > 0 and valasztott <= 4:
+        number1 = float(input("Please enter your first number!: "))
+        number2 = float(input("Please enter your second number!: "))
+        if valasztott == 1:
+            final = number1 + number2
+            print(f"{number1} + {number2} equals: {final}!")
+        elif valasztott == 2:
+            final = number1 - number2
+            print(f"{number1} - {number2} equals: {final}!")
+        elif valasztott == 3:
+            final = number1 * number2
+            print(f"{number1} * {number2} equals: {final}!")
+        elif valasztott == 4:
+            final = number1 // number2
+            print(f"{number1} / {number2} equals: {final}!")
+        else:
+            print("The choosen action is not supported!")
+                    
+def Main():
+    operations()
+    calculator()
 
-
-main()
+Main()
